23483f6f0280c586cfcd177fd549c25d0427157d
HYLee1008 <discone1008@gmail.com>
2020-08-22 13:30:33 +0900

    work out

--- Temporary/card.py
+++ Temporary/card.py
@@ -1,3 +1,5 @@
+import random
+
 ranks = ["Ace", '2', '3', '4', '5', '6', '7', '8', '9', '10', "Jack", "Queen", "King"]
 suits = ["Diamonds", "Hearts", "Clubs", "Spades"]
 
@@ -5,3 +7,5 @@ cards = []
 for rank in ranks:
     for suit in suits:
         cards.append(suit + ' ' + rank)
+
+random.shuffle(cards)
